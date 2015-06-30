ccfac35894da07b96cf4048720dd58b52f7bed7d
Karina Goot <kgoot@berkeley.edu>
2015-06-30 15:39:00 -0700

    test common field population

--- dfaker/common_fields.py
+++ dfaker/common_fields.py
@@ -8,7 +8,7 @@ def add_common_fields(name, datatype, timestamp, zonename):
     """ Populate common fields applicable to all datatypes
         name -- name of datatype
         datatype -- a dictionary for a specific data type
-        timestamp -- an epoch time 
+        timestamp -- an epoch time in utc
         zonename -- name of timezone in effect 
     """
     datatype["type"] = name
@@ -18,5 +18,5 @@ def add_common_fields(name, datatype, timestamp, zonename):
     datatype["timezoneOffset"] = tools.get_offset(zonename, datetime.fromtimestamp(timestamp))
     datatype["deviceTime"] = (time.strftime('%Y-%m-%dT%H:%M:%S', 
                               time.gmtime(timestamp + datatype["timezoneOffset"]*60)))
-    datatype["time"] = (time.strftime('%Y-%m-%dT%H:%M:%S.000Z', time.gmtime(timestamp)))
+    datatype["time"] = time.strftime('%Y-%m-%dT%H:%M:%S.000Z', time.gmtime(timestamp))
     return datatype
