d9042f0aa3de8ff8fd56fbd4305b42dc680cd6c2
Anders Pitman <tapitman11@gmail.com>
2015-10-20 20:58:48 +0000

    Refactoring for flake8 and clarity
    Added some intermediate variables to make some of the time
    conversions more clear.

--- tests/test_common_fields.py
+++ tests/test_common_fields.py
@@ -4,25 +4,28 @@ import unittest
 import dfaker.common_fields as common_fields
 import dfaker.tools as tools
 
+
 class Test_Common_Fields(Chai):
 
     def test_common_fields(self):
         """ Test that common fields populate properly"""
         name = "bolus"
         datatype = {}
-        timestamp = tools.convert_ISO_to_epoch('2015-03-03 00:00:00', '%Y-%m-%d %H:%M:%S')
+        timestamp = tools.convert_ISO_to_epoch('2015-03-03 00:00:00',
+                                               '%Y-%m-%d %H:%M:%S')
         zonename = "US/Pacific"
 
         expected = {
-            'time': '2015-03-03T00:00:00.000Z', #UTC time
-            'deviceTime': '2015-03-02T16:00:00', #local time
+            'time': '2015-03-03T00:00:00.000Z',  # UTC time
+            'deviceTime': '2015-03-02T16:00:00',  # local time
             'timezoneOffset': -480,
             'deviceId': 'DemoData-123456789',
             'uploadId': 'upid_abcdefghijklmnop',
             'conversionOffset': 0,
         }
 
-        result_dict = common_fields.add_common_fields(name, datatype, timestamp, zonename)
+        result_dict = common_fields.add_common_fields(name, datatype,
+                                                      timestamp, zonename)
 
         for key in expected.keys():
             self.assertEqual(result_dict[key], expected[key])
@@ -38,4 +41,3 @@ mySuit = suite()
 
 runner = unittest.TextTestRunner()
 runner.run(mySuit)
-
